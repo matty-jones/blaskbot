bfd4d4e506db657f83f62e6dbd2d7f705e2fd976
Matthew Jones <mattyl.jones@dunelm.org.uk>
2017-08-26 10:58:40 -0600

    Added database configuration options

--- cfg.py
+++ cfg.py
@@ -2,16 +2,22 @@ import os
 
 # Contains BlaskBot's config
 
+# Core interface variables
 HOST = "irc.chat.twitch.tv"
 PORT = 6667
 NICK = os.environ["BOTNICK"]
 PASS = os.environ["BOTAUTH"]
-JOIN = os.environ["BOTCHAT"]
+JOIN = os.environ["BOTCHATTEST"]
 RATE = 1  # Messages per second
 VERB = 5
 
+# Function interface variables
 twitchAPIClientID = os.environ["BOTAPIID"]
 owner = 'Blaskatronic'
 twitterUsername = os.environ["BOTTWIT"]
 opList = []
 opOnlyCommands = ['subscribe']
+
+# Viewer Points Database
+pointsToAward = 1
+awardDeltaT = 60
