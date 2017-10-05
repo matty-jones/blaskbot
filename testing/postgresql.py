49183f655cef20bb85f259b92df0217c17e16bc7
Matthew Jones <mattyl.jones@dunelm.org.uk>
2017-10-04 21:45:16 -0600

    Added a test script to put postgresql through its paces

--- testing/postgresql.py
+++ testing/postgresql.py
@@ -1,19 +1,57 @@
 import psycopg2
 import tinydb
 from datetime import datetime
+from multiprocessing import Process
+import time as T
 
 connection = None
 
-def readNumberOfDrinks(cursor):
+def readRow(extra):
     #cursor.execute("SELECT Drinks FROM Viewers WHERE Name='blaskatronic'")
     #row = cursor.fetchone()
     #print(row)
+    connection = psycopg2.connect(database='testdb', user='blaskbot')
+    cursor = connection.cursor()
     cursor.execute("SELECT * FROM Viewers WHERE Name='blaskatronic'")
     row = cursor.fetchone()
-    print(row)
+    print(extra, row)
+    connection.close()
 
 
-def createTables(cursor):
+def incrementPoints():
+    connection = psycopg2.connect(database='testdb', user='blaskbot')
+    cursor = connection.cursor()
+    while True:
+        cursor.execute("UPDATE Viewers SET Points = Points + 1 WHERE Name='blaskatronic'")
+        connection.commit()
+        readRow("INCREMENT")
+    connection.close()
+
+
+def decrementPoints():
+    connection = psycopg2.connect(database='testdb', user='blaskbot')
+    cursor = connection.cursor()
+    while True:
+        cursor.execute("UPDATE Viewers SET Points = Points - 2 WHERE Name='blaskatronic'")
+        connection.commit()
+        readRow("DECREMENT")
+    connection.close()
+
+
+def getClip():
+    connection = psycopg2.connect(database='testdb', user='blaskbot')
+    cursor = connection.cursor()
+    while True:
+        cursor.execute("SELECT * FROM Clips WHERE ID=1")
+        row = cursor.fetchone()
+        connection.commit()
+        print("CLIP", row)
+    connection.close()
+
+
+def createTables():
+    connection = psycopg2.connect(database='testdb', user='blaskbot')
+    cursor = connection.cursor()
     cursor.execute("CREATE TABLE Clips (ID SMALLINT PRIMARY KEY, URL VARCHAR(70), Author VARCHAR(25));")
     cursor.execute("INSERT INTO Clips VALUES (%s, %s, %s);", (1, 'YawningEnthusiasticTriangleDXAbomb', 'blaskatronic'))
 
@@ -23,17 +61,26 @@ def createTables(cursor):
 
 if __name__ == "__main__":
     try:
-        connection = psycopg2.connect(database='testdb', user='blaskbot')
-        cursor = connection.cursor()
         #createTables(cursor)
-        readNumberOfDrinks(cursor)
+        readRow("INIT")
+        incrementPoints = Process(target=incrementPoints, args=(), daemon=True)
+        decrementPoints = Process(target=decrementPoints, args=(), daemon=True)
+        getClip = Process(target=getClip, args=(), daemon=True)
+        input("ARE YOU READY?!")
+        incrementPoints.start()
+        decrementPoints.start()
+        #getClip.start()
+        T.sleep(0.05)
+        exit()
+        #incrementPoints(cursor)
+        #connection.commit()
+        input("WAIT")
 
     except psycopg2.DatabaseError as e:
         if connection:
             connection.rollback()
         print("Error", e)
         exit()
-
 
     if connection:
         connection.close()
