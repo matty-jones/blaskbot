cc2a4d1bc19b4ca5f02e0c253f8b091d5fdf696f
Matthew Jones <mattyl.jones@dunelm.org.uk>
2017-10-07 11:02:04 -0600

    Updated cfg.py

--- cfg.py
+++ cfg.py
@@ -6,12 +6,12 @@ import os
 HOST = "irc.chat.twitch.tv"  # The twitch IRC, don't change this.
 PORT = 6667                  # The twitch IRC port, don't change this either.
 NICK = os.getenv("BOTNICK")  # The bot's name! Must match the twitch account associated with the bot.
-JOIN = os.getenv("BOTCHAT")  # The channel BlaskBot is to look after (your twitch account name).
+JOIN = os.getenv("BOTCHATTEST")  # The channel BlaskBot is to look after (your twitch account name).
 PASS = os.getenv("BOTAUTH")  # The OAuth for the bot's twitch account (needed so the bot can chat).
 HOSTPASS = os.getenv("HOSTAUTH")  # The OAuth for the host's twitch account (needed for channel SET updates)
 DISCORDPASS = os.getenv("DISCORDAUTH")  # The Token for the discord bot app
 RATE = 1  # Messages per second - Don't reduce this below 1 or your bot will get banned.
-VERB = 5  # Verbosity of outputs while the bot is running (1 - 5).
+VERB = 2  # Verbosity of outputs while the bot is running (1 - 5).
 
 # Function interface variables
 twitchAPIClientID = os.getenv("BOTAPIID")  # The bot needs a clientID for the twitch API
@@ -19,20 +19,21 @@ owner = 'Blaskatronic'  # Dev name (will grant this person op in the bot to help
 twitterUsername = os.getenv("BOTTWIT")  # The name of the twitter channel to pull latest tweets from
 opList = []  # List of ops. You can leave this empty - BlaskBot will populate this list when the stream is live
 opOnlyCommands = ['subscribe']  # Any commands that you want to be op-only (i.e. viewers can't use them)
+skipViewers = [JOIN, NICK, 'doryx', 'fsmimp']  # Place the names of the viewers you'd like to skip in here (skipped viewers do earn blaskoins, but don't earn ranks.
 VLCLUAPASS = os.getenv("VLCLUAPASS")  # This is the password for the LUA HTTP interface created by vlc.
                                       # If you don't know what this is, add 'nowplaying' to the opOnlyCommands
                                       # and never use it.
 
 
 # Channel Stuff
-streamTitle = "TEST_TITLE"
-gameTitle = "Creative"
+streamTitle = "Factorio | Part 3 | Factorio or Fiction?"
+gameTitle = "Factorio"
 # The Twitch API won't let me update communities according to their documentation, so dunno
 # how else to do it.
 #communityList = ['UK_Streamers', 'SupportSmallStreamers', 'VarietyStreaming']
 
 # Viewer Points Database
-pointsToAward = 1  # Award this many points to a viewer...
+pointsToAward = 5  # Award this many points to a viewer...
 awardDeltaT = 60   # ...after they have watched for this many seconds
 currencyName = "BlasKoin"  # Custom currenty name
 ranks = {10: 'Probe',
@@ -54,8 +55,15 @@ ranks = {10: 'Probe',
          1400: 'Dark Archon',
          1500: 'Carrier',
          2000: 'Mothership',
-         2500: 'MissingNo.',
-         3000: 'NaN'}  # Set the ranks to whatever you want! Syntax = points: rankName
+         2500: 'Reaver',
+         3000: 'Browncoat',
+         3500: 'Mercenary',
+         4000: 'Companion',
+         4500: 'Cylon Sympathiser',
+         5000: 'Cylon',
+         5500: 'Final Five',
+         6000: 'MissingNo.',
+         6500: 'NaN'}  # Set the ranks to whatever you want! Syntax = points: rankName
 
 # Minigames
 #  Slot machine
