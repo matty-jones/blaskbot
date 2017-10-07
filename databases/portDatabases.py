8ce0906551d3956e889be9ef444b679eebaf90bc
Matthew Jones <mattyl.jones@dunelm.org.uk>
2017-10-06 22:55:14 -0600

    Added some more stuff to the script to add a new column and update the stuff based on a condition

--- databases/portDatabases.py
+++ databases/portDatabases.py
@@ -23,21 +23,34 @@ from tinydb import TinyDB, Query
 if __name__ == "__main__":
     viewersDB = TinyDB('./blaskatronicViewers.db')
     clipsDB = TinyDB('./blaskatronicClips.db')
+    discordDB = TinyDB('./discordNames.db')
 
     viewerDict = viewersDB.search(Query().name.exists())
     clipDict = clipsDB.search(Query().url.exists())
+    discordDict = discordDB.search(Query().twitchName.exists())
 
     connection = psycopg2.connect(database='blaskatronic', user='blaskbot')
     cursor = connection.cursor()
-    cursor.execute("CREATE TABLE Clips (ID SMALLINT PRIMARY KEY, URL VARCHAR(70), Author VARCHAR(25));")
-    cursor.execute("CREATE TABLE Viewers (ID SMALLINT PRIMARY KEY, Name VARCHAR(25), Points SMALLINT, Rank VARCHAR(25), Multiplier FLOAT, Lurker BIT, TotalPoints SMALLINT, DrinkExpiry TIME, Drinks SMALLINT);")
+    #cursor.execute("CREATE TABLE Clips (ID SMALLINT PRIMARY KEY, URL VARCHAR(70), Author VARCHAR(25));")
+    #cursor.execute("CREATE TABLE Viewers (ID SMALLINT PRIMARY KEY, Name VARCHAR(25), Points SMALLINT, Rank VARCHAR(25), Multiplier FLOAT, Lurker BIT, TotalPoints SMALLINT, DrinkExpiry TIME, Drinks SMALLINT);")
 
-    for index, clip in enumerate(clipDict):
-        cursor.execute("INSERT INTO Clips (ID, " + ', '.join(clip.keys()) + ") VALUES (%s, %s, %s);", tuple([index] + list(clip.values())))
+    #for index, clip in enumerate(clipDict):
+    #    cursor.execute("INSERT INTO Clips (ID, " + ', '.join(clip.keys()) + ") VALUES (%s, %s, %s);", tuple([index] + list(clip.values())))
 
-    for index, viewer in enumerate(viewerDict):
-        viewer['lurker'] = 'B' + str(int(eval(viewer['lurker'].capitalize())))
-        cursor.execute("INSERT INTO Viewers (ID, " + ', '.join(viewer.keys()) + ") VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);", tuple([index] + list(viewer.values())))
+    #for index, viewer in enumerate(viewerDict):
+    #    viewer['lurker'] = 'B' + str(int(eval(viewer['lurker'].capitalize())))
+    #    cursor.execute("INSERT INTO Viewers (ID, " + ', '.join(viewer.keys()) + ") VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);", tuple([index] + list(viewer.values())))
+
+    ### Sorting the Discord name mappings out
+    # First, create the discord column
+    cursor.execute("ALTER TABLE Viewers ADD Discord VARCHAR(25);")
+    # Then, update the discord column to be == the name column
+    cursor.execute("UPDATE Viewers SET Discord = Name;")
+
+    # Now, iterate through the TinyDB and re-update the ones we know there are discord names for
+    for _, user in enumerate(discordDict):
+        cursor.execute("UPDATE Viewers SET Discord = %s WHERE Name = %s;", (user['discordName'], user['twitchName']))
+
 
     cursor.execute("SELECT * FROM Viewers;")
     print(cursor.fetchall())
