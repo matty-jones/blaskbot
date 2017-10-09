976e434414648068c4644b8cafefd5b10fdc1b25
Matthew Jones <mattyl.jones@dunelm.org.uk>
2017-10-08 20:15:04 -0600

    Tested all the functions database calls in the postgresql.py script

--- testing/postgresql.py
+++ testing/postgresql.py
@@ -59,28 +59,98 @@ def createTables():
     cursor.execute("INSERT INTO Viewers VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);", (1, 'blaskatronic', 4847, 'Prelate', 1.0, '1', 4787, datetime.now().time(), 38))
 
 
-if __name__ == "__main__":
-    try:
-        #createTables(cursor)
-        readRow("INIT")
-        incrementPoints = Process(target=incrementPoints, args=(), daemon=True)
-        decrementPoints = Process(target=decrementPoints, args=(), daemon=True)
-        getClip = Process(target=getClip, args=(), daemon=True)
-        input("ARE YOU READY?!")
-        incrementPoints.start()
-        decrementPoints.start()
-        #getClip.start()
-        T.sleep(0.05)
-        exit()
-        #incrementPoints(cursor)
-        #connection.commit()
-        input("WAIT")
+def checkViewerExists():
+    connection = psycopg2.connect(database='testdb', user='blaskbot')
+    cursor = connection.cursor()
+    viewer = 'cheeseman'
+    cursor.execute("SELECT EXISTS (SELECT 1 FROM Viewers WHERE Name='" + viewer + "');")
+    data = cursor.fetchone()
+    print(data[0])
 
-    except psycopg2.DatabaseError as e:
-        if connection:
-            connection.rollback()
-        print("Error", e)
-        exit()
+
+def getPoints(viewer='blaskatronic'):
+    connection = psycopg2.connect(database='testdb', user='blaskbot')
+    cursor = connection.cursor()
+    cursor.execute("SELECT Points FROM Viewers WHERE name='" + viewer + "';")
+    data = cursor.fetchone()
+    print(data)
+    return data[0]
+
+
+def updatePoints():
+    connection = psycopg2.connect(database='testdb', user='blaskbot')
+    cursor = connection.cursor()
+    viewer = 'blaskatronic'
+    currentPoints = getPoints(viewer)
+    readRow("BEFORE =")
+    cursor.execute("UPDATE Viewers SET points=(%s) WHERE name='" + viewer + "';", tuple([currentPoints + 10]))
+    cursor.execute("UPDATE Viewers SET totalpoints=(%s) WHERE name='" + viewer + "';", tuple([currentPoints + 100]))
+    cursor.execute("UPDATE Viewers SET rank=(%s) WHERE name='" + viewer + "';", tuple(['Chump']))
+
+    cursor.execute("UPDATE Viewers SET lurker=(%s) WHERE name='" + viewer + "';", tuple(['B1']))
+    cursor.execute("SELECT Lurker FROM Viewers WHERE name='" + viewer + "';")
+    lurkerStatus = cursor.fetchone()[0]
+    print(bool(int(lurkerStatus)))
+    cursor.execute("UPDATE Viewers SET lurker=(%s) WHERE name='" + viewer + "';", tuple(['B0']))
+    cursor.execute("SELECT Lurker FROM Viewers WHERE name='" + viewer + "';")
+    lurkerStatus = cursor.fetchone()[0]
+    print(bool(int(lurkerStatus)))
+    connection.commit()
+    readRow("AFTER =")
+    newPoints = getPoints(viewer)
+
+
+def updateLurker(viewer='blaskatronic'):
+    connection = psycopg2.connect(database='testdb', user='blaskbot')
+    cursor = connection.cursor()
+    cursor.execute("UPDATE Viewers SET lurker=(%s) WHERE name='" + viewer + "';", tuple(['B0']))
+    cursor.execute("SELECT Lurker FROM Viewers WHERE name='" + viewer + "';")
+    lurkerStatus = cursor.fetchone()[0]
+    print(bool(int(lurkerStatus)))
+    cursor.execute("UPDATE Viewers SET lurker=(%s);", tuple(['B1']))
+    cursor.execute("SELECT Lurker FROM Viewers WHERE name='" + viewer + "';")
+    lurkerStatus = cursor.fetchone()[0]
+    print(bool(int(lurkerStatus)))
+
+
+def getBoth(viewer='blaskatronic'):
+    connection = psycopg2.connect(database='testdb', user='blaskbot')
+    cursor = connection.cursor()
+    cursor.execute("SELECT (points, totalPoints) FROM Viewers WHERE name='" + viewer + "';")
+    data = cursor.fetchone()[0]
+    print(eval(data))
+    (currentPoints, currentTotalPoints) = eval(data)
+    print(currentPoints, currentTotalPoints)
+
+
+if __name__ == "__main__":
+    #try:
+    #    #createTables(cursor)
+    #    readRow("INIT")
+    #    incrementPoints = Process(target=incrementPoints, args=(), daemon=True)
+    #    decrementPoints = Process(target=decrementPoints, args=(), daemon=True)
+    #    getClip = Process(target=getClip, args=(), daemon=True)
+    #    input("ARE YOU READY?!")
+    #    incrementPoints.start()
+    #    decrementPoints.start()
+    #    #getClip.start()
+    #    T.sleep(0.05)
+    #    exit()
+    #    #incrementPoints(cursor)
+    #    #connection.commit()
+    #    input("WAIT")
+
+    #except psycopg2.DatabaseError as e:
+    #    if connection:
+    #        connection.rollback()
+    #    print("Error", e)
+    #    exit()
+
+
+    #checkViewerExists()
+    #updatePoints()
+    #updateLurker()
+    getBoth()
 
     if connection:
         connection.close()
