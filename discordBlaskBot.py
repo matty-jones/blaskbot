2389e40bfaa8f6e1c0aa4449a3c854ddfaf3c46a
Matty Jones <mattyjones@boisestate.edu>
2017-10-10 13:05:16 -0600

    Updated the discord instantiation of BlaskBot to use postgresql

--- discordBlaskBot.py
+++ discordBlaskBot.py
@@ -2,9 +2,7 @@ import cfg
 import discord
 from discord.ext import commands
 import random as R
-from functions import loadClipsDatabase as getClipsDB
-from functions import loadViewersDatabase as getViewersDB
-from tinydb import TinyDB, Query
+import psycopg2
 
 commandPrefix = "!"
 client = commands.Bot(command_prefix=commandPrefix)
@@ -36,10 +34,14 @@ async def dece(context):
 @client.command(pass_context=True)
 async def clip(context):
     '''Display a random clip for everyone's enjoyment.'''
-    clipDB = getClipsDB()
-    clipNo = int(R.randrange(len(clipDB))) + 1
-    author = clipDB.get(eid=clipNo)['author']
-    url = "https://clips.twitch.tv/" + clipDB.get(eid=clipNo)['url']
+    connection = psycopg2.connect(database=_cfg.JOIN.lower(), user=_cfg.NICK.lower())
+    cursor = connection.cursor(cursor_factory=_dictCursor)
+    cursor.execute("SELECT * FROM Clips;")
+    clipList = cursor.fetchall()
+    clipNo = int(_R.randrange(len(clipList)))
+    author = clipList[clipNo]['author']
+    url = "https://clips.twitch.tv/" + clipList[clipNo]['url']
+    connection.close()
     await client.say("Check out this awesome clip from " + author[0].upper() + author[1:] + "! (#" + str(clipNo - 1) + "): " + url)
 
 
@@ -48,49 +50,30 @@ async def blaskoins(context):
     '''Check how many blaskoins you have for the stream minigames.'''
     userName = context.message.author.display_name.lower()
     userNameCap = context.message.author.display_name
-    viewerDB = getViewersDB()
-    discordDB = TinyDB('./databases/discordNames.db')
-    twitchUserName = discordDB.search(Query().discordName == userName)
-    if len(twitchUserName) > 0:
-        userName = twitchUserName[0]['twitchName']
+    connection = psycopg2.connect(database=_cfg.JOIN.lower(), user=_cfg.NICK.lower())
+    cursor = connection.cursor()
+    cursor.execute("SELECT Name FROM Viewers WHERE Discord='" + userName.lower() + "';")
+    userName = str(cursor.fetchone()[0])
     try:
-        currentPoints = viewerDB.search(Query().name == userName)[0]['points']
-        totalPoints = viewerDB.search(Query().name == userName)[0]['totalPoints']
-        currencyUnits = cfg.currencyName
+        cursor.execute("SELECT points FROM Viewers WHERE name='" + userName.lower() + "';")
+        currentPoints = int(cursor.fetchone()[0])
+        cursor.execute("SELECT totalpoints FROM Viewers WHERE name='" + userName.lower() + "';")
+        totalPoints = int(cursor.fetchone()[0])
+        currencyUnits = _cfg.currencyName
         if currentPoints > 1:
             currencyUnits += "s"
-        currentRank = viewerDB.search(Query().name == userName)[0]['rank']
-        currentMultiplier = viewerDB.search(Query().name == userName)[0]['multiplier']
-        nextRank = None
-        pointsForNextRank = None
-        for rankPoints in cfg.ranks.keys():
-            nextRank = cfg.ranks[rankPoints]
-            pointsForNextRank = rankPoints
-            if totalPoints < rankPoints:
-                break
-        secondsToNextRank = (pointsForNextRank - totalPoints) * int(cfg.awardDeltaT /\
-                                (cfg.pointsToAward * currentMultiplier))
-        mins, secs = divmod(secondsToNextRank, 60)
-        hours, mins = divmod(mins, 60)
-        timeDict = {'hour': int(hours), 'minute': int(mins), 'second': int(secs)}
-        timeArray = []
-        for key, value in timeDict.items():
-            if value > 1:
-                timeArray.append(str(value) + " " + str(key) + "s")
-            elif value > 0:
-                timeArray.append(str(value) + " " + str(key))
-        timeToNext = ' and '.join(timeArray[-2:])
-        if len(timeArray) == 3:
-            timeToNext = timeToNext[0] + ", " + timeToNext
-        rankMod = ' '
-        if currentRank[0] in ['a', 'e', 'i', 'o', 'u']:
-            rankMod = 'n '
-        outputLine = "On your twitch account, " + userNameCap + ", you currently have " + str(currentPoints) +\
-                " " + str(currencyUnits) + "!"
-        await client.say(outputLine)
+        cursor.execute("SELECT multiplier FROM Viewers WHERE name='" + userName.lower() + "';")
+        currentMultiplier = float(cursor.fetchone()[0])
+        outputLine = "On your Twitch account, " + userNameCap + ", you currently have " + str(currentPoints) + " " + str(currencyUnits) + "!"
+        if currentMultiplier > 1.01:
+            outputLine += ", with an active bonus of {:.2%}!".format(currentMultiplier - 1)
+        else:
+            outputLine += "!"
+        awaut client.say(outputLine)
     except IndexError:
-        await client.say(sock, "I'm sorry, " + userNameCap + ", but I don't have any " + _cfg.currencyName +\
+        await client.say("I'm sorry, " + userNameCap + ", but I don't have any " + _cfg.currencyName +\
               " data for you yet! Please try again later (and also welcome to the stream ;)).")
+    connection.close()
 
 
 @client.command(pass_context=True)
@@ -98,25 +81,27 @@ async def rank(context):
     '''Check to see how long you've watched BlaskatronicTV for.'''
     userName = context.message.author.display_name.lower()
     userNameCap = context.message.author.display_name
-    viewerDB = getViewersDB()
-    discordDB = TinyDB('./databases/discordNames.db')
-    twitchUserName = discordDB.search(Query().discordName == userName)
-    if len(twitchUserName) > 0:
-        userName = twitchUserName[0]['twitchName']
+    connection = psycopg2.connect(database=_cfg.JOIN.lower(), user=_cfg.NICK.lower())
+    cursor = connection.cursor()
+    cursor.execute("SELECT Name FROM Viewers WHERE Discord='" + userName.lower() + "';")
+    userName = str(cursor.fetchone()[0])
     try:
-        totalPoints = viewerDB.search(Query().name == userName)[0]['totalPoints']
-        currentRank = viewerDB.search(Query().name == userName)[0]['rank']
-        currentMultiplier = viewerDB.search(Query().name == userName)[0]['multiplier']
+        cursor.execute("SELECT totalpoints FROM Viewers WHERE name='" + userName.lower() + "';")
+        totalPoints = int(cursor.fetchone()[0])
+        cursor.execute("SELECT rank FROM Viewers WHERE name='" + userName.lower() + "';")
+        currentRank = str(cursor.fetchone()[0])
+        cursor.execute("SELECT multiplier FROM Viewers WHERE name='" + userName.lower() + "';")
+        currentMultiplier = float(cursor.fetchone()[0])
         nextRank = None
         pointsForNextRank = None
-        for rankPoints in cfg.ranks.keys():
-            nextRank = cfg.ranks[rankPoints]
+        for rankPoints in _cfg.ranks.keys():
+            nextRank = _cfg.ranks[rankPoints]
             pointsForNextRank = rankPoints
             if totalPoints < rankPoints:
                 break
-        secondsToNextRank = (pointsForNextRank - totalPoints) * int(cfg.awardDeltaT /\
-                                (cfg.pointsToAward * currentMultiplier))
-        totalSecondsSoFar = totalPoints * int(cfg.awardDeltaT / cfg.pointsToAward)
+        secondsToNextRank = (pointsForNextRank - totalPoints) * int(_cfg.awardDeltaT /\
+                                (_cfg.pointsToAward * currentMultiplier))
+        totalSecondsSoFar = totalPoints * int(_cfg.awardDeltaT / _cfg.pointsToAward)
         totalMins, totalSecs = divmod(totalSecondsSoFar, 60)
         totalHours, totalMins = divmod(totalMins, 60)
         totalTimeDict = {'hour': int(totalHours), 'minute': int(totalMins), 'second': int(totalSecs)}
@@ -132,7 +117,7 @@ async def rank(context):
                 totalTimeArray.append(str(value) + " " + str(key))
         totalTime = ' and '.join(totalTimeArray[-2:])
         if len(totalTimeArray) == 3:
-            totalTime = totalTime[0] + ", " + totalTime
+            totalTime = totalTimeArray[0] + ", " + totalTime
         for key, value in timeDict.items():
             if value > 1:
                 timeArray.append(str(value) + " " + str(key) + "s")
@@ -140,7 +125,7 @@ async def rank(context):
                 timeArray.append(str(value) + " " + str(key))
         timeToNext = ' and '.join(timeArray[-2:])
         if len(timeArray) == 3:
-            timeToNext = timeToNext[0] + ", " + timeToNext
+            timeToNext = timeArray[0] + ", " + timeToNext
         rankMod = ' '
         if currentRank[0] in ['a', 'e', 'i', 'o', 'u']:
             rankMod = 'n '
@@ -151,26 +136,29 @@ async def rank(context):
     except IndexError:
         await client.say("I'm sorry, " + userNameCap + ", but I don't have any rank" +\
               " data for you yet! Have you checked out the stream yet?")
+    connection.close()
+
 
 @client.command(pass_context=True)
 async def drinks(context):
     '''Check how many drinks you have to enjoy on stream.'''
     userName = context.message.author.display_name.lower()
     userNameCap = context.message.author.display_name
-    viewerDatabase = getViewersDB()
-    discordDB = TinyDB('./databases/discordNames.db')
-    twitchUserName = discordDB.search(Query().discordName == userName)
-    if len(twitchUserName) > 0:
-        userName = twitchUserName[0]['twitchName']
-    numberOfDrinks = int(viewerDatabase.search(Query().name == userName)[0]['drinks'])
+    connection = psycopg2.connect(database=_cfg.JOIN.lower(), user=_cfg.NICK.lower())
+    cursor = connection.cursor()
+    cursor.execute("SELECT Name FROM Viewers WHERE Discord='" + userName.lower() + "';")
+    userName = str(cursor.fetchone()[0])
+    cursor.execute("SELECT drinks FROM Viewers WHERE name='" + userName.lower() + "';")
+    numberOfDrinks = int(cursor.fetchone()[0])
     if numberOfDrinks == 0:
-        await client.say("You don't have any drinks to use on stream, " + userNameCap + "! Maybe a kind soul will buy you one...")
+        await client.say("You don't have any drinks, " + userName + "! Maybe a kind soul will buy you one...")
         return 0
     elif numberOfDrinks == 1:
         drinkString = "1 drink"
     else:
         drinkString = str(numberOfDrinks) + " drinks"
     await client.say("On your twitch account, you have " + drinkString + " to enjoy on stream, " + userNameCap + "!")
+    connection.close()
 
 
 @client.command(pass_context=True)
