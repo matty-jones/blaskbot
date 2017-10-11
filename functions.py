70449d988a7360638425b90b7fe13bc2747772f7
Matthew Jones <mattyl.jones@dunelm.org.uk>
2017-10-11 14:37:54 -0600

    Added discord name to new user addition

--- functions.py
+++ functions.py
@@ -184,7 +184,7 @@ def threadUpdateDatabase(sock):
                             printv("Adding " + viewer + " to database...", 4)
                             insert = {'Name': viewer.lower(), 'Points': 0, 'Rank': 'Chump',
                                       'Multiplier': 1.0, 'Lurker': 'B1', 'TotalPoints': 0,
-                                      'DrinkExpiry': None, 'Drinks': 0}
+                                      'DrinkExpiry': None, 'Drinks': 0, 'Discord': viewer.lower()}
                             cursor.execute("INSERT INTO Viewers (%s) VALUES %s;", (AsIs(', '.join(insert.keys()), tuple(insert.values()))))
                         cursor.execute("SELECT Points FROM Viewers WHERE name='" + viewer.lower() + "';")
                         currentPoints = cursor.fetchone()[0]
